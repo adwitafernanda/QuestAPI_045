f0186c229382da8ef51617787a1c3ad84d923394
adwitafernanda <adwitafernanda4@gmail.com>
2024-12-30 08:00:47 +0700

    Edit - HomeViewModel

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/HomeViewModel.kt
@@ -5,41 +5,41 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.pertemuan12.model.Mahasiswa
 import com.example.pertemuan12.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 import okio.IOException
-import retrofit2.HttpException
 
-sealed class HomeUiState {
-    data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
-    object Error : HomeUiState()
-    object Loading : HomeUiState()
+sealed class  HomeUiState {
+    data class Success(val mahasiswa: List <Mahasiswa>): HomeUiState()
+    object  Error : HomeUiState()
+    object  Loading : HomeUiState()
 }
 
-class HomeViewModel (private val mhs: MahasiswaRepository)
-    : ViewModel(){
-    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+class HomeViewModel (private val mhs: MahasiswaRepository): ViewModel(){
+    var mhsUIState:HomeUiState by mutableStateOf(HomeUiState.Loading)
         private set
 
-    init {
+    init{
         getMhs()
     }
 
-    fun getMhs() {
-        viewModelScope.launch {
-            mhsUiState = HomeUiState.Loading
-            mhsUiState = try {
+    fun getMhs(){
+        viewModelScope.launch{
+            mhsUIState = HomeUiState.Loading
+            mhsUIState = try {
                 HomeUiState.Success(mhs.getMahasiswa())
-            } catch (e: IOException) {
+            }catch (e:IOException){
                 HomeUiState.Error
-            } catch (e: HttpException) {
+            }catch (e:HttpException){
                 HomeUiState.Error
             }
         }
     }
 
-    fun deleteMhs(nim: String) {
+
+    fun deleteMhs(nim:String) {
         viewModelScope.launch {
             try {
                 mhs.deleteMahasiswa(nim)
