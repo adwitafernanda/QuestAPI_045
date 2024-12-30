8273a30ca450e6db4216ebe221e384e07c877473
adwitafernanda <adwitafernanda4@gmail.com>
2024-12-30 07:57:10 +0700

    Edit - TopAppBar

--- app/src/main/java/com/example/pertemuan12/ui/costumwidget/topAppBar.kt
+++ app/src/main/java/com/example/pertemuan12/ui/costumwidget/topAppBar.kt
@@ -15,7 +15,7 @@ import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CoustumeTopAppBar(
+fun TopAppBar(
     title: String,
     canNavigateBack: Boolean,
     modifier: Modifier = Modifier,
