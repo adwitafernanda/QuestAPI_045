6927aeb054c4bb7784f7eb8fae842701c17f93b6
adwitafernanda <adwitafernanda4@gmail.com>
2024-12-30 08:01:11 +0700

    Edit - HomeView

--- app/src/main/java/com/example/pertemuan12/ui/view/HomeView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/HomeView.kt
@@ -17,6 +17,9 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,42 +29,50 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan12.R
 import com.example.pertemuan12.model.Mahasiswa
-import com.example.pertemuan12.ui.costumwidget.CoustumeTopAppBar
+import com.example.pertemuan12.ui.costumwidget.TopAppBar
 import com.example.pertemuan12.ui.navigation.DestinasiNavigasi
 import com.example.pertemuan12.ui.viewmodel.HomeUiState
 import com.example.pertemuan12.ui.viewmodel.HomeViewModel
 import com.example.pertemuan12.ui.viewmodel.PenyediaViewModel
 
-
 object DestinasiHome : DestinasiNavigasi {
-    override val route = "home"
-    override val titleRes = "Home Mhs"
+    override val route = "Home"
+    override val titleRes = "Home Mahasiswa"
 }
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
-    navigateToltemEntry: () -> Unit,
+    navigateToItemEntry: () -> Unit,
     modifier: Modifier = Modifier,
-    onDetailClick: (String) -> Unit = {},
+    onDetailClick:(String) -> Unit = {},
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
-) {
+){
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
-        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        modifier =modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            CoustumeTopAppBar(
+            TopAppBar(
                 title = DestinasiHome.titleRes,
                 canNavigateBack = false,
                 scrollBehavior = scrollBehavior,
@@ -72,20 +83,19 @@ fun HomeScreen(
         },
         floatingActionButton = {
             FloatingActionButton(
-                onClick = navigateToltemEntry,
+                onClick = navigateToItemEntry,
                 shape = MaterialTheme.shapes.medium,
                 modifier = Modifier.padding(18.dp)
-            ){
-                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Kontak")
+            ) {
+                Icon(imageVector = Icons.Default.Add,contentDescription = "Add Kontak")
             }
         },
-    ){ innerPadding ->
+    ) {innerPadding ->
         HomeStatus(
-            homeUiState = viewModel.mhsUiState,
-            retryAction = { viewModel.getMhs() },
-            modifier = Modifier.padding(innerPadding),
-            onDetailClick = onDetailClick, onDeleteClick = {
-                viewModel.deleteMhs(it.nim)
+            homeUiState = viewModel.mhsUIState,
+            retryAction = {viewModel.getMhs()}, modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick, onDeleteClick ={
+                viewModel.deleteMhs(it.nim )
                 viewModel.getMhs()
             }
         )
@@ -95,23 +105,24 @@ fun HomeScreen(
 @Composable
 fun HomeStatus(
     homeUiState: HomeUiState,
-    retryAction: () -> Unit,
+    retryAction:() -> Unit,
     modifier: Modifier = Modifier,
-    onDetailClick: (String) -> Unit,
-    onDeleteClick: (Mahasiswa) -> Unit = {},
+    onDeleteClick:(Mahasiswa) -> Unit = {},
+    onDetailClick: (String) -> Unit
+
 ){
-    when (homeUiState) {
+    when (homeUiState){
         is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
 
         is HomeUiState.Success ->
-            if (homeUiState.mahasiswa.isEmpty()) {
-                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            if(homeUiState.mahasiswa.isEmpty()){
+                return Box(modifier = modifier.fillMaxSize(),contentAlignment = Alignment.Center){
                     Text(text = "Tidak ada data Kontak")
                 }
-            } else {
+            }else {
                 MhsLayout(
-                    mahasiswa = homeUiState.mahasiswa,
-                    modifier = modifier.fillMaxWidth(),
+
+                    mahasiswa = homeUiState.mahasiswa, modifier = modifier.fillMaxWidth(),
                     onDetailClick = {
                         onDetailClick(it.nim)
                     },
@@ -120,12 +131,12 @@ fun HomeStatus(
                     }
                 )
             }
-        is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+        is HomeUiState.Error -> OnError(retryAction,modifier=modifier.fillMaxSize())
     }
 }
 
 @Composable
-fun OnLoading(modifier: Modifier = Modifier) {
+fun OnLoading(modifier: Modifier = Modifier){
     Image(
         modifier = modifier.size(200.dp),
         painter = painterResource(R.drawable.loading),
@@ -135,15 +146,18 @@ fun OnLoading(modifier: Modifier = Modifier) {
 
 @Composable
 fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
-    Column(
+    Column (
         modifier = modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    )  {
+    ){
         Image(
-            painter = painterResource(id = R.drawable.connectionerror),
-            contentDescription = ""
+            painter = painterResource(id = R.drawable.connectionerror), contentDescription = ""
         )
+        Text(text = stringResource(id =R.string.loading_failed),modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
     }
 }
 
@@ -152,21 +166,20 @@ fun MhsLayout(
     mahasiswa: List<Mahasiswa>,
     modifier: Modifier = Modifier,
     onDetailClick: (Mahasiswa) -> Unit,
-    onDeleteClick: (Mahasiswa) -> Unit = {},
+    onDeleteClick: (Mahasiswa) -> Unit = {}
 ){
     LazyColumn(
         modifier = modifier,
-        contentPadding = PaddingValues(16.dp),
+        contentPadding =  PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        items(mahasiswa){kontak ->
+        items(mahasiswa) { kontak ->
             MhsCard(
                 mahasiswa = kontak,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clickable {
-                        onDetailClick(kontak)},
-                onDeleteClick = {
+                    .clickable { onDetailClick(kontak) },
+                onDeleteClick ={
                     onDeleteClick(kontak)
                 }
             )
@@ -180,6 +193,7 @@ fun MhsCard(
     modifier: Modifier = Modifier,
     onDeleteClick: (Mahasiswa) -> Unit = {}
 ) {
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
     Card(
         modifier = modifier,
         shape = MaterialTheme.shapes.medium,
@@ -199,7 +213,7 @@ fun MhsCard(
                     style = MaterialTheme.typography.titleLarge
                 )
                 Spacer(Modifier.weight(1f))
-                IconButton(onClick = { onDeleteClick(mahasiswa) }) {
+                IconButton(onClick = {deleteConfirmationRequired = true}) {
                     Icon(
                         imageVector = Icons.Default.Delete,
                         contentDescription = null,
@@ -220,4 +234,55 @@ fun MhsCard(
             )
         }
     }
+    if (deleteConfirmationRequired) {
+        DeleteConfirmationDialog(
+            onDeleteConfirm = {
+                deleteConfirmationRequired = false
+                onDeleteClick(mahasiswa)
+            },
+            onDeleteCancel =  {
+                deleteConfirmationRequired = false
+            }, modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = onDeleteCancel,
+        title = {
+            Text(
+                "Hapus Data",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        },
+        text = {
+            Text(
+                "Apakah Anda yakin ingin menghapus data ini? Tindakan ini tidak dapat dibatalkan.",
+                fontSize = 16.sp
+            )
+        },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text("Batal", color = MaterialTheme.colorScheme.onSurfaceVariant)
+            }
+        },
+        confirmButton = {
+            Button(
+                onClick = onDeleteConfirm,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            ) {
+                Text("Hapus", color = Color.White)
+            }
+        }
+    )
 }
